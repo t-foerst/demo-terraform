19dc6ea36ed3c329416c83df4347198b9eaeb03f
Thorben <ThorbenFoerst@proton.me>
2026-06-22 16:05:34 +0200

    multi az eks cluster deployed

--- main.tf
+++ main.tf
@@ -1,1 +1,46 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
 
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.small"]
+      min_size       = 2
+      max_size       = 2
+      desired_size   = 2
+    }
+  }
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   default     = "eu-central-1"
   description = "AWS region to deploy resources into"
 }
+
+variable "cluster_name" {
+  type        = string
+  default     = "demo-eks"
+  description = "Name of the EKS cluster"
+}
+
+variable "cluster_version" {
+  type        = string
+  default     = "1.30"
+  description = "Kubernetes version for the EKS cluster"
+}
