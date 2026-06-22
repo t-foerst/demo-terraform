b4e2c0ecca761f642deae2be121752c1f5fe7dc9
Thorben <ThorbenFoerst@proton.me>
2026-06-22 16:43:25 +0200

    IRSA für LBC erstellt

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "cluster_endpoint" {
 output "configure_kubectl" {
   value = "aws eks update-kubeconfig --region ${var.aws_region} --name ${module.eks.cluster_name}"
 }
+
+output "lbc_irsa_role_arn" {
+  value = module.aws_lbc_irsa.iam_role_arn
+}
