96dd9c8ec9c9f24da3e47a33abdec37737d42fdc
Thorben <ThorbenFoerst@proton.me>
2026-08-05 16:34:51 +0200

    instance type verkleinert wegen free tier limit und cluster version erhöht wegen incompatibility

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "cluster_name" {
 
 variable "cluster_version" {
   type        = string
-  default     = "1.30"
+  default     = "1.31"
   description = "Kubernetes version for the EKS cluster"
 }
 
@@ -36,7 +36,7 @@ variable "app_hostnames" {
 
 variable "alb_hostname" {
   type        = string
-  default     = "k8s-democluster-f5f36f3c4f-1964635625.eu-central-1.elb.amazonaws.com" # change this
+  default     = null # change this
   description = "DNS name of the ALB created by the AWS Load Balancer Controller for the cluster Ingress. Leave null until the Ingress exists, then set it to create the CNAMEs."
 }
 
