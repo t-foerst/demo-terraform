49d1066a7fed2c52dc9840a6fa0dd2d05c987ad9
Thorben <ThorbenFoerst@proton.me>
2026-08-05 15:14:22 +0200

    Nat gateway jetzt pro private subnet für HA und instance type auf t3.medium verbessert

--- vpc.tf
+++ vpc.tf
@@ -9,9 +9,10 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+  enable_dns_hostnames   = true
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
